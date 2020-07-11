cea23deaff1dbf5b4bdd1d8aef8f8c2154998a27
akhilkm <akhilu011@gmail.com>
2020-07-11 13:11:56 +0530

    updated

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-repo_project=bis-backend-app
 
-export repo_project=${repo_project}
+export repo_project=bis-backend-app
 export DeploymentTime=$(date +%F--%H-%M-%S--%Z)
+
+export bis_backend_app_replicas=1
+export bis_backend_app_backbone="dotnet EPIC.BIS.API.Core.dll"
+export ACR_ENDPOINT=bisdevelop.azurecr.io
+export image="bisdevelop.azurecr.io/bis-backend-core-app:5"
+export K8S_NAMESPACE="bis-develop"
+
+
+envsub < ./K8s-Infra/K8s-Deployment/bis-backend-app-deployment.yaml
