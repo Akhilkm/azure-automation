05555d209455bc382815cddf833d556928ce9c9f
Akhil K M <akhilu011@gmail.com>
2020-07-25 09:55:56 +0530

    updated

--- DevOps_Scripts/create-devops-resources.sh
+++ DevOps_Scripts/create-devops-resources.sh
@@ -6,7 +6,7 @@ az devops login --organization kuberiter
 
 az pipelines create --name temp-frontend-app --description 'Pipeline for temp-frontend-app project' \
                     --repository temp-frontend-app --branch dev --repository-type tfsgit \
-                    --project EPIC-TestRun --organization https://dev.azure.com/kuberiter/ \
+                    --project PJT-TestRun --organization https://dev.azure.com/kuberiter/ \
                     --skip-first-run true --yaml azure-pipelines.yml
 
 
--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ export repo_project=temp-backend-app
 export DeploymentTime=$(date +%F--%H-%M-%S--%Z)
 
 export temp_backend_app_replicas=1
-export temp_backend_app_backbone="dotnet EPIC.TEMP.API.Core.dll"
+export temp_backend_app_backbone="dotnet PJT.TEMP.API.Core.dll"
 export ACR_ENDPOINT=tempdevelop.azurecr.io
 export image="tempdevelop.azurecr.io/temp-backend-core-app:5"
 export K8S_NAMESPACE="temp-develop"
